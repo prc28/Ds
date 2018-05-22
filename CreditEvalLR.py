e1e070c496ccbf8d8f51fbc3f2639d6cdd486e60
prc28 <priyanshu18.pc@gmail.com>
2018-05-22 15:34:31 +0530

    Update CreditEvalLR.py

--- CreditEvalLR.py
+++ CreditEvalLR.py
@@ -1,4 +1,7 @@
 
+#This is the program to find the regression coefficients of the given data and also to predict the outcomes, in order to get a regression equation 
+#The dependent variables are numerically stored in the MarksX matrix, while the experimented(rather random outputs) stored in the CreditY, in order to implement a credit evaluation system of performance by students
+
 from sklearn import linear_model
 
 MarksX = [[24,29], [34,42], [21,19], [27,36], [38,21], [23,19], [12,16], [19,40], [16,20], [37,23], [24, 45], [20,18], [45, 30], [42, 37], [28, 24], [26,29], [25, 17], [39, 43], [41, 46], [48, 33]]
@@ -10,10 +13,9 @@ regr.fit(MarksX, CreditY)
 
 Y_predict = regr.predict(MarksX)
 
-#print(Y_predict)
+print(Y_predict)
+
 print("Original ",regr.coef_)
 
-#regr.fit(MarksX, Y_predict)
 
-#print("Revised ", regr.coef_)
 print(regr.intercept_)
